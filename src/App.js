88c3cb3c5cf4bfb55eab59f5ca5bc008e3a26b10
tvicky7x <tvicky7x@gmail.com>
2023-09-15 11:32:44 +0530

    favicon done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className=" text-center">Vicky 1</h1>
+      <h1 className=" text-center">Vicky favicon</h1>
       <svg
         viewBox="0 0 24 24"
         className=" fill-violet-800 h-3 w-3"
