4f778ec52f5da1cccd63873b0893afa07db174cf
tvicky7x <tvicky7x@gmail.com>
2023-09-15 21:26:09 +0530

    bg color change

--- src/Components/Container/MainContainer.js
+++ src/Components/Container/MainContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 function MainContainer({ children }) {
   return (
     <div className=" w-screen h-screen sm:flex sm:items-center sm:justify-center py-10 px-4">
-      <div className=" bg-gradient-to-b from-sky-500 to-blue-800 w-full h-full max-w-5xl p-1 rounded-2xl grid sm:grid-rows-none grid-rows-12 sm:grid-flow-col sm:grid-cols-12 overflow-y-hidden">
+      <div className=" bg-gradient-to-b from-cyan-400 to-blue-800 w-full h-full max-w-5xl p-1 rounded-2xl grid sm:grid-rows-none grid-rows-12 sm:grid-flow-col sm:grid-cols-12 overflow-y-hidden">
         {children}
       </div>
     </div>
